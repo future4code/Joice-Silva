734dd320bae87367e5ff453484a3ac216deb7c4c
joice-silva <joice.jss2016@gmail.com>
2021-05-07 16:09:14 -0300

    Adicionando lista-logiga

---
+++ semana1/lista-logica/lista-exercicios-js/exercicios.js
@@ -0,0 +1,132 @@
+// Exemplos
+
+// Exercício 0A
+
+function soma(num1, num2) {
+   // implemente sua lógica aqui
+   return num1 + num2
+}
+
+// Exercício 0B
+
+function imprimeMensagem() {
+   // implemente sua lógica aqui
+   const mensagem = prompt('Digite uma mensagem!')
+
+   console.log(mensagem)
+}
+
+// Exercícios
+
+//Exercício 1
+
+function calculaAreaRetangulo() {
+   // implemente sua lógica aqui
+  let altura = Number(prompt("qual é a altura do retângulo?"))
+  let largura = Number(prompt("qual é a largura do retângulo?"))
+  let areaDoTriangulo = altura * largura
+
+   console.log(`Area do retângulo é igual a:`, areaDoTriangulo)
+
+}
+
+//Exercício 2
+
+function imprimeIdade() {
+   // implemente sua lógica aqui
+}
+
+//Exercício 3
+
+function calculaIMC(peso, altura) {
+   // implemente sua lógica aqui
+   return (Number(peso)/(Number(altura) * Number(altura))
+   
+   console.log("O seu IMC é", IMC))
+
+}
+
+//Exercício 4
+
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+   let nome = prompt("Qual é o seu nome?")
+   let idade = prompt("Qual é sua idade?")
+   let email = prompt("Qual é o seu email?")
+
+   console.log(`Meu nome é ${nome}, tenho ${idade}, e meu e-mail é ${email}`)
+
+}
+
+//Exercício 5
+
+function imprimeTresCoresFavoritas() {
+   // implemente sua lógica aqui
+   const imprimeTresCoresFavoritas = [] ;
+   cost cor1 = prompt("Qual sua primeira cor favorita?");
+   cost cor2 = prompt("Qual sua segunda cor favorita?");
+   cost cor3 = prompt("Qual sua terceira cor favorita?");
+   imprimeTresCoresFavoritas.push(cor1);
+   imprimeTresCoresFavoritas.push(cor2);
+   imprimeTresCoresFavoritas.push(cor3);
+}
+
+//Exercício 6
+
+function retornaStringEmMaiuscula(string) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 7
+
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 8
+
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+}
+
+// Exercício 9
+
+function retornaPrimeiroElemento(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 10
+
+function retornaUltimoElemento(array) {
+   // implemente sua lógica aqui
+}
+
+//Exercício 11
+
+function trocaPrimeiroEUltimo(array) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 12
+
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 13
+
+function checaRenovacaoRG() {
+   // implemente sua lógica aqui
+}
+
+// Exercício 14
+
+function checaAnoBissexto(ano) {
+   // implemente sua lógica aqui
+}
+
+// Exercício 15
+
+function checaValidadeInscricaoLabenu(pessoa) {
+   // implemente sua lógica aqui
+}
