bde228756099805639619728c51f8899479781de
joice-silva <joice.jss2016@gmail.com>
2021-05-12 16:57:32 -0300

    adicionando loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,59 @@
+//............Escrita de código......................
+
+/* 
+ 
+1. Pergunte ao usuário quantos bichinhos de estimação ele
+ tem e guarde esse dado em uma variável.
+
+a) Se a quantidade for 0, imprima no console:
+ "Que pena! Você pode adotar um pet!"
+
+b) Se a quantidade for maior que 0, solicite que o usuário 
+digite os nomes deles, um por um, e guarde esses nomes em um array.
+
+c) Por fim, imprima o array com os nomes dos bichinhos no console.
+
+*/
+
+//................exercicio 1°.......................................
+
+/*let bichinhoEstimacao = ' '
+let bichinhoEstimacaoDigitado;
+let numero
+let soma = 0
+
+while (bichinhoEstimacaoDigitado !== bichinhoEstimacao) {
+    bichinhoEstimacaoDigitado = prompt('Qual Bichinho de estimação você tem?')
+    alert('Que pena! Você pode adotar um')
+}
+*/
+
+/*
+function retornaMaiorNumero(arrayDeNumeros) {
+    let maiorNumero = 0
+    for(let i = 0; i < arrayDeNumeros.length; i++) {
+        if(arrayDeNumeros [i] > maiorNumero) {
+            maiorNumero = arrayDeNumeros [i]
+        }
+    }
+    return maiorNumero
+}
+
+const maiorNumeroDoArray = retornaMaiorNumero([1, 20])
+
+*/
+
+let quantidadeDePet = Number(prompt('Quantos Bichinhos de estimação você tem?'))
+let quantidadeDePets = []
+if (quantidadeDePet === 0) {
+    console.log('Que pena! Você pode adotar um pet!')
+} else if(quantidadeDePet > 0) {
+    for (let i = 0; <= quantidadeDePet; i++) {
+        pets[i] = prompt('Nome do Pet', ${i+i}:)
+
+    }
+}
+
+console.log(quantidadeDePets)
+
+const arrayOriginal = [1, 2, 3, 4, 5]
