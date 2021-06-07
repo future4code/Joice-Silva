6e1c6dba00e18f32ea0fae75f5ff75a9a7d3aec5
joice-silva <joice.jss2016@gmail.com>
2021-06-07 17:01:48 -0300

    LabenuForms

---
+++ semana6/renderizacao-condicional/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import LabenuForms from "./components/Etapa1";
+
+function App() {
+  return (
+    <div className="LabenuForms">
+      <header className="LabenuForms-header">
+        <a
+          className="LabenuForms-link"
+          href="https://m.facebook.com/388131481831991/photos/a.388131531831986/575260359785768/?type=3"
+          target="_blank"
+          rel=""
+        >
+          LabenuForms
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
