3cc0e5f6485962157547325ae39897aaa6c22a65
joice-silva <joice.jss2016@gmail.com>
2021-06-09 17:52:16 -0300

    atualizando lista de tarefas

--- semana6/boiler-plate/boiler-plate/src/index.js
+++ semana6/boiler-plate/boiler-plate/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App'
+
+import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
