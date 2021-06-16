18a5d2d4cf21fa68efac2e7cfa4c338892762360
joice-silva <joice.jss2016@gmail.com>
2021-06-16 04:04:04 -0300

    atualizando

--- semana7/labenusers/src/App.js
+++ semana7/labenusers/src/App.js
@@ -1,6 +1,48 @@
 import React from 'react'
 import './App.css';
 import axios from 'axios';
+import Button from '../src/Components/Button'
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    
+    
+  }
+
+  Container {
+  display: flex;
+
+  align-items: center;
+
+  margin: 20px;
+  font-size: large;
+  
+ 
+  }
+
+  container2{
+    align-items: center;
+    
+    margin: 100px;
+  }
+  
+  
+`;
+
+const AppContainer = styled.div`
+  display: flex;
+  background-color: white;
+  
+  border: solid 3px;
+  max-width: 300px;
+  padding: 40px;
+  
+  margin: 120px auto 0 ;
+`;
+
 
 
 
@@ -13,12 +55,41 @@ export default class App extends React.Component {
   state = {
     nome: "",
     email: "",
-    login: []
+    Button: "", 
   }
   render() {
-    return(
-      <div></div>
-    )
+    return (
+      <AppContainer>
+        
+        <GlobalStyle/>
+
+        <form>
+          <div1>
+          
+            <container>Nome: 
+            <input for ="" type="text" value=""/>
+            </container>
+
+          
+            <container>E-mail: 
+            <input for="" type="text" value=""/>
+            </container>
+          
+          
+
+         
+            <container2>
+            <input type="submit" value="Salvar" />
+            </container2>
+
+          </div1>
+            
+        </form>
+
+      </AppContainer>
+
+        
+    );
   }
 } 
   
---
+++ semana7/labenusers/src/Components/Button.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  border-radius: 50px;
+  width: 200px;
+  padding: 15px 30px;
+  margin: 10px auto;
+  
+`;
+
+
