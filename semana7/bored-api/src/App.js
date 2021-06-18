45c7aa559f690a0ed5d455e79c0a76efd7bc12d2
joice-silva <joice.jss2016@gmail.com>
2021-06-18 01:12:02 -0300

    boredAPI

--- semana7/bored-api/src/App.js
+++ semana7/bored-api/src/App.js
@@ -2,21 +2,24 @@ import "./styles.css";
 import React from "react";
 import axios from "axios";
 
+
+
 export default class App extends React.Component {
   state = {
-  activity: "Learn Express.js",
-  accessibility: 0.25,
-	type: "education",
-	participants: 1,
-	price: 0.1,
-	link: "https://expressjs.com/",
-	key: "3943506"
+    activity: []
 
   }
 
   getActivity = (event) => {
     const url = "https://www.boredapi.com/api/activity/"
     const body = {
+      activity: "Learn Express.js",
+      accessibility: 0.25,
+      type: "education",
+      participants: 1,
+      price: 0.1,
+      link: "https://expressjs.com/",
+      key: "3943506"
 
     }
     axios
@@ -26,21 +29,23 @@ export default class App extends React.Component {
           }
       })
       .then((res) => {
-        console.log(res.data);
+        
         this.setState({activity: res.data})
       })
       .catch((err) => {
         console.log(err);
       });
   };
+
+
   render() {
     const {activity, type, participants, price } = this.state.activity
     return (
       <div className="App">
-        <h1>Ainda não decidi</h1>
-        <button onClick={this.getActivity}>Clique aqui!</button>
+        <div className="Text"><h1 >E-COMMERCE MOLINA</h1></div>
+        <div className="Button"><button onClick={this.getActivity}>Próximo!</button></div>
         <hr />
-        <h3>Atividade</h3>
+        <h3>Sales Platform!</h3>
         <p>Nome: {activity} </p>
         <p>Tipo: {type}</p>
         <p>Participantes: {participants} </p>
@@ -49,4 +54,3 @@ export default class App extends React.Component {
     );
   }
 }
- 
