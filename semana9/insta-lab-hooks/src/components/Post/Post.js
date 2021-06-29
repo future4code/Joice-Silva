4484e0bcaea503dd21d08c056186f86a4349079d
joice-silva <joice.jss2016@gmail.com>
2021-06-29 11:02:00 -0300

    hooks-resolvido

--- semana9/insta-lab-hooks/src/components/Post/Post.js
+++ semana9/insta-lab-hooks/src/components/Post/Post.js
@@ -9,42 +9,76 @@ import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 
 const Post = (props) => {
+  
+  const [like, setLike] = useState(false)
+  const [numberOfLikes, setnumberOfLikes] = useState(0);
 
+  const [sendComment, setSendComment] = useState(false);
+
+  const [numberOfComments, setNumberOfComments] = useState(0);
+
+  const [allComents, setAllComments] = useState([]);
 
   const onClickCurtida = () => {
+    if (like) {
+      setLike(false);
+      setnumberOfLikes(0);
+    } else {
+      setLike(true);
+      setnumberOfLikes(1);
+    }
   };
 
   const onClickComentario = () => {
+    setSendComment (!sendComment);
   };
 
   const enviarComentario = (comentario) => {
-  }
+    setNumberOfComments(numberOfComments + 1);
+    const newListOfComments = [...allComents, comentario];
+    setSendComment(false);
+    setAllComments(newListOfComments);
+  };
+
+  const iconeCurtida = like ? iconeCoracaoPreto : iconeCoracaoBranco;
+
+  const commentSection = sendComment ? (
+    <SecaoComentario enviarComentario={enviarComentario} />
+  ) : (
+    allComents.map((comentario) => {
+      return (
+        <CommentContainer>
+          <p>{comentario}</p>
+        </CommentContainer>
+      );
+    })
+  );
 
   return (
     <PostContainer>
       <PostHeader>
-        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <UserPhoto src={props.fotoUsuario} alt={"Imagem do usuario"} />
         <p>{props.nomeUsuario}</p>
       </PostHeader>
 
-      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+      <PostPhoto src={props.fotoPost} alt={"Imagem do post"} />
 
       <PostFooter>
         <IconeComContador
-          // icone={iconeCurtida}
+          icone={iconeCurtida}
           onClickIcone={onClickCurtida}
-          // valorContador={numeroCurtidas}
+          valorContador={numberOfLikes}
         />
 
         <IconeComContador
           icone={iconeComentario}
           onClickIcone={onClickComentario}
-          // valorContador={numeroComentarios}
+          valorContador={numberOfComments}
         />
       </PostFooter>
-      {/* {caixaDeComentario} */}
+      {commentSection}
     </PostContainer>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
